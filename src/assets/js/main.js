d7de2368821a569e50e4305ef653f1084b083665
mr cod3r <mrcod3r.ir@gmail.com>
2023-02-06 23:14:21 +0330

    enable flkty slider cdn & disable gsap & page latyou 90% completed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === "development") {
 	require("./devonly/dev");
 }
 
-// require("./slider");
+require("./slider");
 // require("./gsap");
 // Mobile menu
 var openMenuButton = document.getElementById("openMobileMenu"),
@@ -18,30 +18,3 @@ var openMenuButton = document.getElementById("openMobileMenu"),
 // closeMenuButton.addEventListener("click", () =>
 // 	mobileMenu.classList.add("hidden")
 // );
-
-let sections = gsap.utils.toArray(".section");
-let rowHeight = 0;
-
-document.querySelectorAll(".section").forEach((el, i) => {
-	let hH = el.getBoundingClientRect().width;
-	// console.log(hH)
-	rowHeight = rowHeight + hH;
-});
-
-// set window width for pin-spacer
-window.onload = function () {
-	// alert('Page is loaded');
-	document.querySelector(".pin-spacer").classList.add("!mx-0");
-};
-
-gsap.to(sections, {
-	xPercent: -100 * (sections.length - 1),
-	ease: "none",
-	scrollTrigger: {
-		trigger: ".hScroll",
-		pin: true,
-		scrub: 1,
-		// snap: 1 / (sections.length - 1),
-		end: "+=" + (rowHeight - window.innerWidth),
-	},
-});
--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,89 +1,98 @@
-// initial flickity slider on elm
-const initFlkty = function (elm) {
-	// the passed arg (elm) is main.slider tag
-	var flkty = new Flickity(elm, {
-		// options
-		contain: true,
-		pageDots: false,
-		prevNextButtons: false,
-		draggable: false,
-		watchCSS: true,
-	});
+const first_section = document.querySelector("main section:first-child");
+const repositionSlider = () => {
+	const myInterval = setInterval(() => {
+		flickity_2.reposition();
+		console.log("myInterval....");
+	}, 10);
+	setTimeout(() => {
+		clearInterval(myInterval);
+	}, 210);
+};
 
-	// trigger slider by mouse wheel
-	elm.addEventListener("wheel", (e) => {
-		if (sliderEnable) {
-			console.log("slider working...");
-			e.preventDefault();
-			if (e.deltaY > 0) {
-				// prev slide
-				flkty.previous();
+// use x and y mousewheel event data to navigate flickity
+function flickity_handle_wheel_event(
+	e,
+	flickity_instance,
+	flickity_is_animating
+) {
+	// do not trigger a slide change if another is being animated
+	if (!flickity_is_animating) {
+		// pick the larger of the two delta magnitudes (x or y) to determine nav direction
+		var direction =
+			Math.abs(e.deltaX) > Math.abs(e.deltaY) ? e.deltaX : e.deltaY;
+
+		// console.log("wheel scroll ", e.deltaX, e.deltaY, direction);
+
+		if (direction > 0) {
+			// next slide
+			if (
+				flkty_selected_target == 0 &&
+				first_section.classList.contains("shrink") == false
+			) {
+				// if is first trigger of mouse and section has not shrink class prevent next slide and add shrink class
+				// document.querySelector("main.slider").classList.add("animate");
+				first_section.classList.add("shrink");
+
+				// repositionSlider();
+				flickity_2.reposition();
+			} else if (
+				flkty_selected_target == 0 &&
+				first_section.classList.contains("shrink") == true
+			) {
+				flickity_instance.next();
 			} else {
-				// next slide
-				flkty.next();
+				flickity_instance.next();
 			}
 		} else {
-			console.log("slider not working...");
-		}
-	});
-};
-// main slider wrapper  that will be observed for mutations
-var sliderWrp = document.querySelector("main.slider");
-// boolean for toggle slider
-var sliderEnable = false;
+			// prev slide
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-	for (const mutation of mutationList) {
-		if (mutation.type === "attributes") {
-			sliderEnable = true;
-			initFlkty(mutation.target);
-			canActive = false;
-			// Later, you can stop observing
-			observer.disconnect();
+			if (flkty_selected_target == 0) {
+				// first section must be full width
+				first_section.classList.remove("shrink");
+				// document.querySelector("main.slider").classList.remove("animate");
+				flickity_2.reposition();
+				// repositionSlider();
+			}
+			flickity_instance.previous();
 		}
 	}
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(sliderWrp, config);
-
-// bool for toggle slider activator
-var canActive = true;
-
-// slider activator element
-const activatorElm = document.querySelector(".sliderActivator");
-
-// slider activator fn
-const activator = (e) => {
-	console.log(e);
-	if (canActive) {
-		console.log("slider activator working...");
-		if (e.deltaY > 0) {
-			// remove enable class from sliderWrp
-			sliderWrp.classList.remove("enable");
-		} else {
-			// add enable class to sliderWrp
-			sliderWrp.classList.add("enable");
-			activatorElm.classList.remove("enable");
-		}
-	} else {
-		console.log("slider activator not work");
-	}
-};
-
-// if element exist
-if (activatorElm != null) {
-	activatorElm.addEventListener("wheel", (e) => {
-		activator(e);
-	});
-	activatorElm.addEventListener("keyup", (e) => {
-		activator(e);
-	});
 }
+
+// second carousel
+var carousel_2 = document.querySelector("main.slider");
+var flickity_2 = new Flickity("main.slider", {
+	contain: true,
+	pageDots: false,
+	prevNextButtons: true,
+	draggable: false,
+	wrapAround: false,
+	selectedAttraction: 0.01,
+	friction: 0.15,
+	// watchCSS: true,
+	on: {
+		ready: function () {
+			// console.log("Flickity is ready");
+		},
+		change: function (index) {
+			// console.log("Slide changed to" + index);
+			flkty_selected_target = index;
+		},
+	},
+});
+var flickity_2_is_animating = 0;
+var flkty_selected_target = 0;
+flickity_2.on("settle", function (index) {
+	// console.log("Slide settle " + index);
+	flickity_2_is_animating = false;
+	// console.log("flkty_selected_target =", flkty_selected_target);
+});
+
+flickity_2.on("select", function (index) {
+	// console.log("Slide selected " + index);
+	flickity_2_is_animating = true;
+});
+
+// detect mousewheel event within carousel element
+carousel_2.onwheel = function (e) {
+	flickity_handle_wheel_event(e, flickity_2, flickity_2_is_animating);
+};
